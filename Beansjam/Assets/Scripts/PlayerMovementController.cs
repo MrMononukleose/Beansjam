56a620be1a68c60ada8ae3c7cf5d6ee2a0e7dc53
Ulrich Schmidt <Uli.Schmidt88@web.de>
2018-10-13 00:44:44 +0200

    Hintergrund + PlayerMovement

---
+++ Beansjam/Assets/Scripts/PlayerMovementController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security.Policy;
+using UnityEngine;
+
+public class PlayerMovementController : MonoBehaviour
+{
+    public string HorizontalAxisName;
+
+    public string VerticalAxisName;
+
+    public float Speed = 10;
+
+    private void FixedUpdate()
+    {
+        var horizontalInput = Input.GetAxisRaw(HorizontalAxisName);
+        var verticalInput = Input.GetAxisRaw(VerticalAxisName);
+
+        var directionVector = new Vector2(horizontalInput, verticalInput);
+
+        GetComponent<Rigidbody2D>().velocity = directionVector.normalized * Speed;
+    }
+}
